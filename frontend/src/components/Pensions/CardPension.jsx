8264777a6fa1a9a40c21768113e86b9edc760581
valentin <valentingueret06@gmail.com>
2023-02-02 11:49:31 +0100

    ok

--- frontend/src/components/Pensions/CardPension.jsx
+++ frontend/src/components/Pensions/CardPension.jsx
@@ -1,16 +1,33 @@
-import React from "react";
-// import axios from "axios";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "../../styles/CardPension.scss";
 
 function CardPension() {
-  // const [animaux, setAnimaux] = useState;
+  const [pensions, setPensions] = useState([]);
 
-  // const getPensions = () => {
-  //   axios.get("http://localhost:5000/Pensions").then((res) => {
-  //     setAnimaux(res.results[0]);
-  //   });
-  // };
+  const getPensions = () => {
+    axios.get("http://localhost:5000/Pensions").then((res) => {
+      setPensions(res.data);
+    });
+  };
 
-  return <div className="CardPension">;</div>;
+  useEffect(() => {
+    getPensions();
+  }, []);
+
+  return (
+    <div className="pensions">
+      <ul className="pensions-list">
+        {pensions.map((pension) => (
+          <li key={pension.id} className="card-pension">
+            <img src="./backend/image-animaux-default.jpg" alt="img_pensions" />
+            <p>{pension.nb_animal_prst}</p>
+            <p>{pension.grandeur}m </p>
+            <p>{pension.type} </p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
-
 export default CardPension;
